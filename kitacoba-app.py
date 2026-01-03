25f7edcfe02bd69d1f9e8b608bbcbfe773a07662
gillandshofwan <gillandshofwan456@gmail.com>
2026-01-03 16:31:41 +0700

    Add image display for sunrise by the mountains

--- kitacoba-app.py
+++ kitacoba-app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.write("Hello, *World!* :sunglasses:")
+
+import streamlit as st
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu8J1grqbFq9oOO1bf9Xudncl-rMBDb-4rNLsq0Q8FsxiFwmaH_if1xDOORRkyumxa3kSxQ_X-EsCYhobo-c7OIVuXaLlc84d7RWaNses&s", caption="Sunrise by the mountains")
