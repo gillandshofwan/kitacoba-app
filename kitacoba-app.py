a4ffc5849206cb34231917a0c6a2e62a865e5186
gillandshofwan <gillandshofwan456@gmail.com>
2026-01-03 17:39:22 +0700

    Refactor sidebar options and add image
    Removed selectbox and radio button from sidebar, added an image.

--- kitacoba-app.py
+++ kitacoba-app.py
@@ -2,15 +2,4 @@ import streamlit as st
 
 st.write("Hello, *World!* :sunglasses:")
 
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
+st.image("sunrise.jpg", caption="Sunrise by the mountains")
