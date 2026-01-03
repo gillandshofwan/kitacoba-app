87a77cb9b845453a6c7e6abafa27303afb317b03
gillandshofwan <gillandshofwan456@gmail.com>
2026-01-03 18:06:02 +0700

    Modify button implementation in sidebar

--- kitacoba-app.py
+++ kitacoba-app.py
@@ -5,7 +5,7 @@ st.write("Hello, *World!* :sunglasses:")
 st.image("Methylene Blue dengan UV tanpa ZnO.png", caption="Sunrise by the mountains")
 
 with st.sidebar:
-    with st.button("Reset", type="primary")
+    with st.button() :
           if st.button("Say hello"):
             st.write("Why hello there")
       
