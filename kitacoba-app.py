d72544e18322c33b0c3d0c73ec59986cb85cdee9
gillandshofwan <gillandshofwan456@gmail.com>
2026-01-03 18:04:46 +0700

    Update kitacoba-app.py

--- kitacoba-app.py
+++ kitacoba-app.py
@@ -3,4 +3,9 @@ import streamlit as st
 st.write("Hello, *World!* :sunglasses:")
 
 st.image("Methylene Blue dengan UV tanpa ZnO.png", caption="Sunrise by the mountains")
-st.sidebar.[Daftar Info]
+
+with st.sidebar:
+    with st.button("Reset", type="primary")
+          if st.button("Say hello"):
+            st.write("Why hello there")
+      
