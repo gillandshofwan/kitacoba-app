ac2bd57b0bf4dcd3bd6be7d9265c64bce13e299c
gillandshofwan <gillandshofwan456@gmail.com>
2026-01-03 16:33:57 +0700

    Change image source to local file path

--- kitacoba-app.py
+++ kitacoba-app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.write("Hello, *World!* :sunglasses:")
 
 import streamlit as st
-st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu8J1grqbFq9oOO1bf9Xudncl-rMBDb-4rNLsq0Q8FsxiFwmaH_if1xDOORRkyumxa3kSxQ_X-EsCYhobo-c7OIVuXaLlc84d7RWaNses&s", caption="Sunrise by the mountains")
+st.image("C:\Users\HYPE AMD\Pictures\Screenshots\Screenshot 2025-10-09 142547.png", caption="Sunrise by the mountains")
