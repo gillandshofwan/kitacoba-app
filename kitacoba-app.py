a2fda98e003faa030ab0f72c3ed3a62d66d480c4
gillandshofwan <gillandshofwan456@gmail.com>
2026-01-04 15:02:00 +0700

    Update sidebar title and greeting message

--- kitacoba-app.py
+++ kitacoba-app.py
@@ -2,11 +2,9 @@ import streamlit as st
 
 st.write("Hello, *World!* :sunglasses:")
 
-
-st.sidebar.title("Menu")
-st.sidebar.write("Selamat datang di website saya")
-
 st.title("Halaman Utama")
 st.write("Ini konten utama")
 
-      
+st.sidebar.title("Mau Hitung Apa Hari Ini?")
+st.sidebar.write("Selamat datang di website saya")
+
