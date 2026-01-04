8cbf114eddadfd331418e04631977246f73e1d88
gillandshofwan <gillandshofwan456@gmail.com>
2026-01-04 15:09:12 +0700

    Refactor sidebar and main title logic

--- kitacoba-app.py
+++ kitacoba-app.py
@@ -5,6 +5,17 @@ st.write("Hello, *World!* :sunglasses:")
 st.title("Halaman Utama")
 st.write("Ini konten utama")
 
-st.sidebar.title("Mau Hitung Apa Hari Ini?")
-st.sidebar.write("Selamat datang di website saya")
+st.sidebar.title("Mau_Hitung_Apa_Hari_Ini?")
+Mau_Hitung_Apa_Hari_Ini? = st.sidebar.selectbox(
+    "Pilih Menu:",
+    ["Pilihan 1", "Pilihan 2"]
+)
+
+st.title(Mau_Hitung_Apa_Hari_Ini?)
+
+if Mau_Hitung_Apa_Hari_Ini? == "Pilihan 1":
+    st.write("Konten Pilihan 1")
+
+elif Mau_Hitung_Apa_Hari_Ini? == "Pilihan 2":
+    st.write("Konten Pilihan 2")
 
