c48343043c5f37e0ee5eecc26b62db354e7c5219
gillandshofwan <gillandshofwan456@gmail.com>
2026-01-04 14:57:40 +0700

    Refactor layout and content in kitacoba-app.py
    Removed image and sidebar button, added titles and welcome message.

--- kitacoba-app.py
+++ kitacoba-app.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 st.write("Hello, *World!* :sunglasses:")
 
-st.image("Methylene Blue dengan UV tanpa ZnO.png", caption="Sunrise by the mountains")
 
-with st.sidebar:
-    with st.button() :
-          if st.button("Say hello"):
-            st.write("Why hello there")
+st.sidebar.title("Menu")
+st.sidebar.write("Selamat datang di website saya")
+
+st.title("Halaman Utama")
+st.write("Ini konten utama")
+
       
